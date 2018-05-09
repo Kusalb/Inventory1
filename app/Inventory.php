697e38f0bdb1596061b54bbefeab0274efa4e9d5
abhishek-a19k <abhishek.a19k@gmail.com>
2018-05-09 16:18:08 +0545

    Inventory

---
+++ app/Inventory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    protected $table = 'inventory';
+    protected $guarded =[
+
+
+    ];
+}
